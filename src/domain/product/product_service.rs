a5dc81e68acfcee2a03caccd41156bb9a5b23b4d
AmirHosein Mardani <139256057+this-ILECY@users.noreply.github.com>
2025-09-04 13:31:58 +0330

    fix error

--- src/domain/product/product_service.rs
+++ src/domain/product/product_service.rs
@@ -1,10 +1,16 @@
-use crate::domain::product::product_entity::{Entity as Product};
+use crate::domain::product::product_entity::{Entity as Product, Model as ProductModel};
 use sea_orm::{DatabaseConnection, EntityTrait};
 
 pub struct ProductService;
 
 impl ProductService {
-    // pub async fn exists_by_id(db: &DatabaseConnection, id: i32) -> Result<bool, sea_orm::DbErr> {
-    //     Ok(Product::find_by_id(id).one(db).await?.is_some())
-    // }
+    pub async fn exists_by_id(db: &DatabaseConnection, id: i32) -> Result<bool, sea_orm::DbErr> {
+        Ok(Product::find_by_id(id).one(db).await?.is_some())
+    }
+
+    pub async fn get_all_products(
+        db: &DatabaseConnection,
+    ) -> Result<Vec<ProductModel>, sea_orm::DbErr> {
+        Product::find().all(db).await
+    }
 }
