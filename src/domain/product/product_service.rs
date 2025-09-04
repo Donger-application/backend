ac1299827ce3bfda9506105a32a62010dc498089
AmirHosein Mardani <139256057+this-ILECY@users.noreply.github.com>
2025-09-04 13:27:52 +0330

    merge stuff!

--- src/domain/product/product_service.rs
+++ src/domain/product/product_service.rs
@@ -4,7 +4,7 @@ use sea_orm::{DatabaseConnection, EntityTrait};
 pub struct ProductService;
 
 impl ProductService {
-    pub async fn exists_by_id(db: &DatabaseConnection, id: i32) -> Result<bool, sea_orm::DbErr> {
-        Ok(Product::find_by_id(id).one(db).await?.is_some())
-    }
+    // pub async fn exists_by_id(db: &DatabaseConnection, id: i32) -> Result<bool, sea_orm::DbErr> {
+    //     Ok(Product::find_by_id(id).one(db).await?.is_some())
+    // }
 }
